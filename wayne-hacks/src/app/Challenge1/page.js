b49f66a0a29b43dd9836ec5e2bda393db1643bca
Lynn Hakim <hk9794@wayne.edu>
2024-01-13 14:27:12 -0500

    question files

---
+++ wayne-hacks/src/app/Challenge1/page.js
@@ -0,0 +1,11 @@
+export default function Challenge1(){
+    return (
+        <div>
+
+            <h2 className="font-bold text-lg">Challenge 1</h2>
+            <h3>Create an ascending number staircase with 10 rows </h3>
+        
+        </div>
+
+    );
+}
---
+++ wayne-hacks/src/app/Print/page.js
@@ -0,0 +1,18 @@
+export default function Print(){
+    return (
+        <div>
+
+        <body>
+        
+        <h2 className="font-bold text-lg">Question 1: Print</h2>
+        <h3>Print "Hello World"</h3>
+        
+        
+        </body>
+        
+        </div>
+
+    );
+
+   
+}
