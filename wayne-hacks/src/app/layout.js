9f242058055debbe964c8db93171ebb3c5dc27e4
Lynn Hakim <hk9794@wayne.edu>
2024-01-13 17:10:57 -0500

    commit

--- wayne-hacks/src/app/layout.js
+++ wayne-hacks/src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,10 +10,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
-    </ClerkProvider>
   );
 }
