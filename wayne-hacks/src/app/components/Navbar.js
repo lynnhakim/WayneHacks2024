0a621c263badd1760c5afabcb8d03432c33bd487
Merwin aamir <merwinaamir1@gmail.com>
2024-01-13 14:23:24 -0500

    added navbar to home

--- wayne-hacks/src/app/components/Navbar.js
+++ wayne-hacks/src/app/components/Navbar.js
@@ -1,3 +1,17 @@
 export default function Navbar() {
-  return <div className="bg-blue-500 w-screen">Helloas</div>;
+  return (
+    <div className="flex bg-white h-16 items-center z-40">
+      <div className="flex text-sm items-center mr-5 justify-end w-full">
+        <p className="mx-3 text-gray font-bold hover:underline cursor-pointer">
+          Score
+        </p>
+        <p className="hidden md:block mx-3 text-gray font-bold hover:underline cursor-pointer">
+          LeaderBoard
+        </p>
+        <p className="hidden md:block mx-3 text-gray font-bold hover:underline cursor-pointer">
+          Profile
+        </p>
+      </div>
+    </div>
+  );
 }
