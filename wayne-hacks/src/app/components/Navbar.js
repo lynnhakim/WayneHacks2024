ea7486e9354f869513d19e7ebd17dd410b47e1b5
shawiminhas <shawiminhas@hotmail.com>
2024-01-13 21:19:02 -0500

    changed colors

--- wayne-hacks/src/app/components/Navbar.js
+++ wayne-hacks/src/app/components/Navbar.js
@@ -3,7 +3,7 @@ import Profile from "../Profile/page";
 Profile
 export default function Navbar({Score}) {
   return (
-    <div className="flex bg-gray-100 h-16 items-center z-40">
+    <div className="flex bg-black h-16 items-center z-40">
       <HomeIcon className="h-8 w-8 ml-5" />
       <div className="flex text-sm items-center mr-5 justify-end w-full">
         <a href="/Score">
